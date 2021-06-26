f13dc3d9ff50bcc3b3ad0e7927b44e8f00ee857c
Juan Sebastian Solano Aviles <jssolano585@misena.edu.co>
2021-06-26 00:29:01 -0500

    Version primera

---
+++ Main.js
@@ -0,0 +1,7 @@
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-cards', { delay: 100 });
+ScrollReveal().reveal('.card_1', { delay: 100 });
+ScrollReveal().reveal('.card_2', { delay: 100 });
+ScrollReveal().reveal('.card_3', { delay: 100 });
+ScrollReveal().reveal('.card_4', { delay: 100 });
+ScrollReveal().reveal('.banner_2', { delay: 100 });
